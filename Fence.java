223457bfb18e9169ecc65453406c948ed39b5a25
Harrison Frahn <harrisonfrahn@gmail.com>
2016-09-07 19:37:33 -0700

    testing git in eclipse

--- Fence.java
+++ Fence.java
@@ -1,8 +1,8 @@
 public class Fence extends Tile{
 	public Fence(int x, int y){
-		super(x, y, "src/rsz_test.jpg");
+		super(x, y, "rsz_test.jpg");
 	}
 	public Fence(){
-		super(0, 0, "src/rsz_test.jpg");
+		super(0, 0, "rsz_test.jpg");
 	}
 }
--- Game.java
+++ Game.java
@@ -15,7 +15,7 @@ public class Game extends JPanel{
 		Game g = new Game();
 		JFrame j = new JFrame();
 
-		j.setSize(620, 640);
+		j.setSize(600, 600);
 		j.add(g);
 		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
 		j.setVisible(true);
@@ -32,7 +32,7 @@ public class Game extends JPanel{
 		g.fillRect(0, 0, 600, 600);
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File("src/rsz_test.jpg"));
+		    img = ImageIO.read(new File("rsz_test.jpg"));
 		} catch (IOException e) {
 		}
 		for(int y = 0; y < 12; y++){
