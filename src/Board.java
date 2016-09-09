2f11b1c9cc54747c8a42105464d91dbdbccaad12
engtechp7 <engtechp7@199.80.144.173>
2016-09-09 10:11:42 -0700

    added randomized fences

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,9 @@
 package src;
+import java.util.Random;
 public class Board{
 	public Tile [][] board = new Tile [12][12];
 	public Board(){
+		Random r = new Random();
 		for(int y = 0; y < 12; y++){
 			for(int x = 0; x < 12; x++){
 				if(x==0||x==11||y==0||y==11){
@@ -12,7 +14,15 @@ public class Board{
 				}
 			}
 		}
-		
+		for(int i = 0; i < 20; i++){
+			int x = r.nextInt(11);
+			int y = r.nextInt(11);
+			while(board[y][x] instanceof Fence){
+				 x = r.nextInt(11);
+				 y = r.nextInt(11);
+			}
+			board[y][x] = new Fence();
+		}
 	}
 	public static void main(String [] args){
 		Board b = new Board();
