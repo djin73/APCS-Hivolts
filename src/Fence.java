122ef714e6122be305ba8b0683e8e746ceafa119
Harrison Frahn <harrisonfrahn@gmail.com>
2016-09-23 10:11:51 -0700

    found a better fence image

--- src/Fence.java
+++ src/Fence.java
@@ -1,9 +1,9 @@
 package src;
 public class Fence extends Tile{
 	public Fence(int x, int y){
-		super(x, y, "images/rsz_test.jpg");
+		super(x, y, "images/fence.png");
 	}
 	public Fence(){
-		super(0, 0, "images/rsz_test.jpg");
+		super(0, 0, "images/fence.png");
 	}
 }
